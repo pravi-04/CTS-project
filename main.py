593cd2fb1b3bb7558420ec047f666fa6554770d9
pravi-04 <pravinaak44@gmail.com>
2024-04-04 16:41:16 +0530

    'code'

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+# Delete this line: word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+

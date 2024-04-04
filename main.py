d7753c778641fa0e7740c3b7316cc8cf1ed65cc8
megha1126 <107837999+megha1126@users.noreply.github.com>
2024-04-04 16:50:00 +0530

    start the game.py

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
+from hangman_art import logo
+
+print(logo)
+
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
  if guess in display:
         print(f"You've already guessed {guess}")
 
